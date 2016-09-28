99d383f070f562510f3d0aa4b2dbeb76281789dc
Jorge Liano <jorgeliano@gmail.com>
2016-09-27 19:53:38 -0700

    Changed directive to allow dynamic

--- src/js/pdf-viewer.js
+++ src/js/pdf-viewer.js
@@ -8,9 +8,10 @@ angular.module('pdf')
       restrict: 'E',
       template: '<pdf-viewer-toolbar ng-if="showToolbar" delegate-handle="{{id}}" page-count="pageCount"></pdf-viewer-toolbar><canvas></canvas>',
       scope: true,
+      delegateHandle: '@',
       controller: 'PdfCtrl',
       link: function(scope, element, attrs) {
-        scope.id = attrs.delegateHandle;
+        scope.id = scope.delegateHandle;
         scope.showToolbar = scope.$eval(attrs.showToolbar) || false;
       }
     };
